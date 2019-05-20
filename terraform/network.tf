9276a7b8ec44d08bf12ad8c8870fed28200d0819
lauwnch <jdkrueger12@gmail.com>
2019-05-20 11:55:24 -0400

    changed private subnet gateway type to NAT

--- terraform/network.tf
+++ terraform/network.tf
@@ -67,8 +67,8 @@ resource "aws_route_table" "private" {
   vpc_id = "${ aws_vpc.project_network.id }"
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${ aws_nat_gateway.app.id }"
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${ aws_nat_gateway.app.id }"
   }
 }
 
